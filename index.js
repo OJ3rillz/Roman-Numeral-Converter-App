32f95825022d05978514187581deb16ad301187f
OJ3rillz <timitrilli@gmail.com>
2019-05-16 13:25:26 +0100

    add  completion update

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ function convertToRoman(num) {
             D: 500,
             CD: 400,
             C: 100,
-            
-      }
+            XC: 90,
+            L: 50,
+            XL: 40,
+            X: 10,
+            IX: 9,
+            V: 5,
+            IV: 4,
+            I: 1
+      };
 
       var roman = "";
       for (var key in romanToNum) {
             console.log(key)
+            // console.log("value: " , romanToNum[key]);
+            while (num >= romanToNum[key]) {
+                  roman += key;
+                  num -= romanToNum[key];
+                  console.log("Num is decreased: ", num)
+            }
       }
+      return roman;
 }
 
-
-
-convertToRoman(36);
+convertToRoman(3);
