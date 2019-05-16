6aa4589f0498f93d380e9d71f0b501db95e9f255
OJ3rillz <timitrilli@gmail.com>
2019-05-16 12:46:23 +0100

    add key value pairs

---
+++ index.js
@@ -0,0 +1,19 @@
+function convertToRoman(num) {
+      var romanToNum = {
+            M: 1000,
+            CM: 900,
+            D: 500,
+            CD: 400,
+            C: 100,
+            
+      }
+
+      var roman = "";
+      for (var key in romanToNum) {
+            console.log(key)
+      }
+}
+
+
+
+convertToRoman(36);
